02b9de3afe3a03c9dee60376cdd0f2c0009a350b
kriss <462679766@qq.com>
2018-01-05 17:09:56 +0800

    fix: 无用代码

--- backend/views/position/_create_update.php
+++ backend/views/position/_create_update.php
@@ -1,12 +1,11 @@
 <?php
 /** @var $this \yii\web\View */
-/** @var $model Position */
+/** @var $model \common\models\Position */
 
 use backend\widgets\SimpleAjaxForm;
-use common\models\Position;
-
-$position = Position::findIdName(true);
 
 $form = SimpleAjaxForm::begin(['header' => ($model->isNewRecord) ? '新增' : '修改']);
+
 echo $form->field($model, 'name');
+
 SimpleAjaxForm::end();
