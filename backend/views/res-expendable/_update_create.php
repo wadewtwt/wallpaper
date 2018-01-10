6bf716f01030ac5f972acbb71de6a0bf847e97a4
wadewt <1078210460@qq.com>
2018-01-10 18:36:56 +0800

    fix:消耗品、设备更新操作部分字段不可编辑

--- backend/views/res-expendable/_update_create.php
+++ backend/views/res-expendable/_update_create.php
@@ -6,6 +6,6 @@ use backend\widgets\SimpleAjaxForm;
 $form = SimpleAjaxForm::begin(['header' => ($model->isNewRecord) ? '新增' : '编辑']);
 echo $form->field($model, 'name');
 echo $form->field($model, 'min_stock');
-echo $form->field($model, 'current_stock');
-echo $form->field($model, 'scrap_cycle');
+echo $form->field($model, 'current_stock')->textInput(['readonly'=>'true']);
+echo $form->field($model, 'scrap_cycle')->textInput(['readonly'=>'true']);
 SimpleAjaxForm::end();
