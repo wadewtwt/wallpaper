f04c661cfb9bfec2f56ea96faad7d04a2673941d
wadewt <1078210460@qq.com>
2018-01-30 16:27:50 +0800

    fix:摄像头管理的修改

--- backend/views/camera/index.php
+++ backend/views/camera/index.php
@@ -5,6 +5,7 @@
 
 use backend\widgets\SimpleDynaGrid;
 use yii\helpers\Html;
+use common\models\Camera;
 
 $this->title = '仓库列表';
 $this->params['breadcrumbs'] = [
@@ -19,6 +20,9 @@ echo $this->render('_search', [
 $columns = [
     [
         'attribute' => 'store_id',
+        'value' => function (Camera $model) {
+            return $model->store->name;
+        }
     ],
     [
         'attribute' => 'ip',
