595c8a3954614cc1205514a8f91f058e4b4c0b1c
kriss <462679766@qq.com>
2018-02-07 22:32:38 +0800

    fix: select2 small

--- backend/views/apply-order/_search.php
+++ backend/views/apply-order/_search.php
@@ -1,6 +1,5 @@
 <?php
 /** @var $this yii\web\view */
-
 /** @var $model backend\models\ApplyOrderSearch */
 
 use backend\widgets\SimpleSearchForm;
@@ -13,6 +12,18 @@ echo $form->field($model, 'person_id')->dropDownList(Person::findAllIdName(true)
     'prompt' => '请选择'
 ])->label('申请人');
 
+echo $form->field($model, 'status')->dropDownList(ApplyOrder::$statusData, [
+    'prompt' => '请选择'
+]);
+echo $form->field($model, 'status')->dropDownList(ApplyOrder::$statusData, [
+    'prompt' => '请选择'
+]);
+echo $form->field($model, 'status')->dropDownList(ApplyOrder::$statusData, [
+    'prompt' => '请选择'
+]);
+echo $form->field($model, 'status')->dropDownList(ApplyOrder::$statusData, [
+    'prompt' => '请选择'
+]);
 echo $form->field($model, 'status')->dropDownList(ApplyOrder::$statusData, [
     'prompt' => '请选择'
 ]);
