7f271572c69ed714f75a1027c9d7b488b8e2fc46
wadewt <1078210460@qq.com>
2018-02-10 18:31:51 +0800

    fix:操作日志管理的规范搜索的设置

--- backend/views/operate-log/_more.php
+++ backend/views/operate-log/_more.php
@@ -5,7 +5,7 @@
 use backend\widgets\SimpleAjaxForm;
 use common\models\Admin;
 
-$form = SimpleAjaxForm::begin(['header' => '更多信息']);
+$form = SimpleAjaxForm::begin(['header' => '更多信息','renderSubmit'=>false,'cancelLabel' => '返回']);
 
 echo $form->field($model, 'created_at')->textInput(['readonly' => true, 'value' => date("Y-m-d H:i:s", $model->created_at)]);
 echo $form->field($model, 'created_by')->dropDownList(Admin::findAllIdName(true), ['readonly' => true]);
@@ -13,6 +13,4 @@ echo $form->field($model, 'route')->textInput(['readonly' => true]);
 echo $form->field($model, 'absolute_url')->textInput(['readonly' => true]);
 echo $form->field($model, 'method')->textInput(['readonly' => true]);
 
-$form->renderSubmit = false;
-$form->cancelLabel = '返回';
 SimpleAjaxForm::end();
